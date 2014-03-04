fb994cb769273f4ab52ad277f43cf381026a8ebb
Daniel Mecke <daniel.mecke@cunningsoft.de>
2014-03-04 14:53:50 +0100

    initial commit

---
+++ geodata.js
@@ -0,0 +1,14 @@
+var level1 = ['europe', 'northamerica', 'southamerica', 'asia', 'africa', 'oceania'];
+
+var level2 = {};
+level2.europe = ['germany', 'france'];
+level2.northamerica = [];
+level2.southamerica = [];
+level2.asia = [];
+level2.africa = [];
+level2.oceania = [];
+
+var level3 = {};
+level3.europe = {};
+level3.europe.germany = ['schleswigholstein', 'hamburg', 'niedersachsen'];
+level3.europe.france = [];
