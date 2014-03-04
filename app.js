26b8db1279949fd48698ed0775f9517ffa871bdc
Daniel Mecke <daniel.mecke@cunningsoft.de>
2014-03-04 14:55:27 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,77 +1,67 @@
 $(document).ready(function() {
-    log('document ready');
+    initDom();
 });
 
-function log(message) {
-    console.log(message);
-    $('#log').append('<div>' + message + '</div>');
+function checkin() {
+    navigator.geolocation.getCurrentPosition(onSuccess);
 }
 
-// Wait for device API libraries to load
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// device APIs are available
-function onDeviceReady() {
-    log('device is ready');
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
-
-// onSuccess Geolocation
 function onSuccess(position) {
-    log('geolocation onSuccess');
     var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'latLng': position}, function(results, status) {
-        log('status: ' + status);
+//    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var latlng = new google.maps.LatLng($('#latitude').val(), $('#longitude').val());
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            log(results);
-            if (results[0]) {
-                var loc = getCountry(results);
-                log(loc);
-            }
+            console.log('country: ' + findElement(results, 'country').join());
+            console.log('administrative_area_level_1: ' + findElement(results, 'administrative_area_level_1').join());
+            console.log('administrative_area_level_2: ' + findElement(results, 'administrative_area_level_2').join());
+            console.log('administrative_area_level_3: ' + findElement(results, 'administrative_area_level_3').join());
+            console.log('locality: ' + findElement(results, 'locality').join());
+            console.log('sublocality: ' + findElement(results, 'sublocality').join());
+            console.log('neighborhood: ' + findElement(results, 'neighborhood').join());
+            console.log('political: ' + findElement(results, 'political').join());
+
+            $('#country_' + findElement(results, 'country')[0]).addClass('solved');
         }
     });
-
-    $('#geolocation').html('Test - Latitude: '           + position.coords.latitude              + '<br />' +
-        'Longitude: '          + position.coords.longitude             + '<br />' +
-        'Altitude: '           + position.coords.altitude              + '<br />' +
-        'Accuracy: '           + position.coords.accuracy              + '<br />' +
-        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-        'Heading: '            + position.coords.heading               + '<br />' +
-        'Speed: '              + position.coords.speed                 + '<br />' +
-        'Timestamp: '          + position.timestamp                    + '<br />' +
-        'Country: '            + loc                                   + '<br />');
 }
 
-// onError Callback receives a PositionError object
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-        'message: ' + error.message + '\n');
-}
-
-function getCountry(results)
-{
-    for (var i = 0; i < results[0].address_components.length; i++)
-    {
-        var shortname = results[0].address_components[i].short_name;
-        var longname = results[0].address_components[i].long_name;
-        var type = results[0].address_components[i].types;
-        if (type.indexOf("country") != -1)
-        {
-            if (!isNullOrWhitespace(shortname))
-            {
-                return shortname;
-            }
-            else
-            {
-                return longname;
-            }
+function findElement(results, type) {
+    elements = [];
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].types.indexOf(type) != -1) {
+            elements.push(results[i].address_components[0].long_name);
         }
     }
+
+    return elements;
 }
 
-function isNullOrWhitespace(text) {
-    if (text == null) {
-        return true;
+function initDom() {
+    $('#list').append(createList('world', 'level1', level1));
+    for (var i = 0; i < level1.length; i++) {
+        $('#list').append(createList(level1[i], 'level2', level2[level1[i]]));
+        for (var j = 0; j < level2[level1[i]].length; j++) {
+            $('#list').append(createList(level1[i] + '_' + level2[level1[i]][j], 'level3', level3[level1[i]][level2[level1[i]][j]]));
+        }
     }
-    return text.replace(/\s/gi, '').length < 1;
+
+    $('.level1 li').click(function() {
+        $('.level2').hide();
+        $('.level3').hide();
+        $('.level2.' + $(this).attr('id').substr(13)).show();
+    });
+    $('.level2 li').click(function() {
+        $('.level3').hide();
+        $('.level3.' + $(this).attr('id').substr(7)).show();
+    });
+}
+
+function createList(base, level, elements) {
+    var list = $('<ul class="' + level + ' ' + base + '"></ul>');
+    for (var i = 0; i < elements.length; i++) {
+        list.append($('<li id="' + level + '_' + base + '_' + elements[i] + '">' + elements[i] + '</li>'));
+    }
+
+    return list;
 }
